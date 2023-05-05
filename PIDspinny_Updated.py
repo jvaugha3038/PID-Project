9e23481f55e3b5dc0c9ae8b6c50d6eb458a2b044
Avanhoo <113116247+Avanhoo@users.noreply.github.com>
2023-05-05 09:56:53 -0400

    Update PIDspinny_Updated.py

--- PIDspinny_Updated.py
+++ PIDspinny_Updated.py
@@ -9,7 +9,7 @@ import pwmio
 
 # get and i2c object
 i2c = board.I2C()
-fan = pwmio.PWMOut(board.D10, duty_cycle=0, frequency=440, variable_frequency=True)
+fan = pwmio.PWMOut(board.A1, duty_cycle=0, frequency=440, variable_frequency=True)
 # some LCDs are 0x3f... some are 0x27.
 
 mpu = adafruit_mpu6050.MPU6050(i2c)
@@ -20,13 +20,17 @@ print("hey")
 time.sleep(1)
 lcd.clear()
 #setting up stuff
-encoder = rotaryio.IncrementalEncoder(board.D2, board.D3, divisor=2)
-button = digitalio.DigitalInOut(board.D4)
+encoder = rotaryio.IncrementalEncoder(board.D1, board.D2, divisor=2)
+button = digitalio.DigitalInOut(board.D3)
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
+# on/off switch setup
+switch = digitalio.DigitalInOut(board.D8)
+switch.direction = digitalio.Direction.INPUT
+switch.pull = digitalio.Pull.UP
 
-deg = 0
 #subtract 12.9 degrees
+#variable soup
 KP = 1
 KI = 1
 KD = 1
@@ -34,7 +38,7 @@ encoder.position = 0
 menu = 1
 m_edit = False
 last_position = -2
-Set = 45
+Set = 0
 dt = .1
 prev = 0
 deg = -12.9
@@ -43,7 +47,7 @@ op = 0
 P = 0
 I = 0
 D = 0
-up = 1
+#defining the pid function
 def pid(Set,ierr,dt,KP,KI,KD):
         global prev
         global deg
@@ -53,17 +57,16 @@ def pid(Set,ierr,dt,KP,KI,KD):
         ophi = 100
         oplo = 10
         # calculate the error
-        print("prev = "+str(prev))
         print("deg = "+str(deg))
         prev = deg
         
         deg=(round(float(mpu.gyro[0])+0.038, 1)*(dt)*(180/3.14159))+prev
-        
+        # calculate the measurement derivative
         dpv = (deg - prev) / dt
         error = Set-deg
         # calculate the integral error
         ierr = ierr + KI * error * dt
-        # calculate the measurement derivative
+
 
         # calculate the PID output
         P = KP * error
@@ -76,38 +79,37 @@ def pid(Set,ierr,dt,KP,KI,KD):
             # clip output
             op = max(oplo,min(ophi,op))
         # return the controller output and PID terms
-        return [op,P,I,D,prev]
+        return op
 
 while True:
+    print(pid(Set,ierr,dt,KP,KI,KD))
+    fan.duty_cycle = int(pid(Set,ierr,dt,KP,KI,KD)*65535//100)
 
-    print(str(pid(Set,ierr,dt,KP,KI,KD)))
-    
     position = encoder.position
-    
     if position > last_position: # Changes the PID values if edit mode is on, changes the menu if edit mode is off
         if m_edit == True:
             if menu == 1:
                 KP += 1
             elif menu == 2:
-				KI += 1
-			elif menu == 3:
-				KD += 1
+                KI += 1
+            elif menu == 3:
+                KD += 1
         else:
             menu+=1
     elif position < last_position:
-		if m_edit == True:
-			if menu == 1:
+        if m_edit == True:
+            if menu == 1:
                 KP -= 1
             elif menu == 2:
-				KI -= 1
-			elif menu == 3:
-				KD -= 1
+                KI -= 1
+            elif menu == 3:
+                KD -= 1
         else:
-        	menu-=1
-        
+            menu-=1
+
     if menu == 0: # Stops the menu from going too far
-        menu = 4
-    elif menu > 4:
+        menu = 3
+    elif menu > 3:
         menu = 1
 
 # checks which page is selected
@@ -119,8 +121,8 @@ while True:
             lcd.print("kI = "+str(KI))
         if menu == 3:
             lcd.print("kD = "+str(KD))
-		if m_edit == True:
-			lcd.print("      Editing ^v")
+        if m_edit == True:
+            lcd.print("          Editing ^v")
 
     if not button.value:   # Toggles the edit mode
        if m_edit == False:
